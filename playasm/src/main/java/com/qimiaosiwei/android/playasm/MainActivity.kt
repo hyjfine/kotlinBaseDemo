abe38bf3b0895cb9a864fbb57d51af4f84dc57e7
heyongjian <kevin.he@ximalaya.com>
2021-12-06 17:53:47 +0800

    add module playAsm

---
+++ playasm/src/main/java/com/qimiaosiwei/android/playasm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.qimiaosiwei.android.playasm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
